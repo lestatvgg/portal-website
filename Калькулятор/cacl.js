18d6e8c5ad4ab2464795b3721265a4f0b435db46
Hp <phoenix_vgg@mail.ru>
2021-11-25 19:57:14 +0300

    first redactor

---
+++ Калькулятор/cacl.js
@@ -0,0 +1,51 @@
+const textarea = document.querySelector('#numpad-textarea')
+
+
+
+
+
+
+const enterNumber = (number) => {
+        textarea.value = textarea.value + number
+}
+
+//сложение
+const sumNumbers = () => {
+        if (!textarea.value || textarea.value[textarea.value.length - 1] === '+') return
+        if (textarea.value.includes('+')) {
+                const numbersArr = textarea.value.split ('+')
+                textarea.value = parseInt(numbersArr[0]) + parseInt(numbersArr[1])
+        }
+        textarea.value = textarea.value + '+'
+}
+
+
+//вычитание
+const subNumbers = () => {
+        if (!textarea.value || textarea.value[textarea.value.length - 1] === '-') return
+        if (textarea.value.includes('-')) {
+                const numbersArr = textarea.value.split ('-')
+                textarea.value = parseInt(numbersArr[0]) - parseInt(numbersArr[1])
+        }
+        textarea.value = textarea.value + '-'
+}
+
+//умножение
+const multiNumbers = () => {
+        if (!textarea.value || textarea.value[textarea.value.length - 1] === '*') return
+        if (textarea.value.includes('*')) {
+                const numbersArr = textarea.value.split ('*')
+                textarea.value = parseInt(numbersArr[0]) - parseInt(numbersArr[1])
+        }
+        textarea.value = textarea.value + '*'
+}
+
+
+
+// const buttons = document.querySelectorAll('.number')
+//
+// console.log({buttons});
+//
+// for (let i=0; i< buttons.length; i++) {
+//     buttons[i].addEventListener('Click', () => enterNumber(buttons[i].innerText))
+// }
